9138911ac0fff4de20916311ab32848fa4aac3ab
n3k0girl <87582918+n3k0girl@users.noreply.github.com>
2021-08-28 09:36:41 +0200

    Source files of Simple Token Checker in CPP

---
+++ src/checker.h
@@ -0,0 +1,6 @@
+#ifndef CHECKER_H
+#define CHECKER_H
+
+void tokenverif();
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <curl/curl.h>
+#include <iostream>
+#include <windows.h>
+#include <json/json.h>
+#include <WinSock2.h>
+#include <vector>
+#include "checker.h"
+
+int main()
+{
+	tokenverif();
+}
